d6ff91d3abd468e7f14c4e1464674f80c2a65cc3
Juan <jusnherraiz82@gmail.com>
2026-04-24 10:03:30 +0200

    reestructuracion de codigo simple

--- Entidades/Inversor.py
+++ Entidades/Inversor.py
@@ -1,8 +1,8 @@
 class Inversor:
-    def __init__(self, nombre, id_usuario):
+    def __init__(self, nombre: str, id_usuario: int):
         self._nombre = nombre
         self._id_usuario = id_usuario
-        self._carteras = []  # Lista de objetos de tipo Cartera
+        self._carteras: list['Cartera'] = []  # Lista de objetos de tipo Cartera [cite: 20, 39]
 
     def agregar_cartera(self, cartera: 'Cartera'):
         self._carteras.append(cartera)
